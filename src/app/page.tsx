534804ff433fa57d1b7a69ff0c263dadc43a95fd
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-09-19 15:58:59 +0200

    t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <main className="flex flex-col justify-center items-center h-screen">
       <h1 className="h-24">BikeTrackr</h1>
       <div className="flex gap-5 items-center ">
-        <Link href="/velos" className="border-2 border-red-700 h-24 w-24">
+        <Link href="/velos" className="border-2 border-red-700 ">
           <Image src="/velo.png" alt="velo" height={100} width={100}></Image>
         </Link>
-        <div className="flex flex-col border-2 border-red-700 gap-5 h-24 w-24">
+        <div className="flex flex-col border-2 border-red-700 gap-5 h-24">
           <Link href="/signup" className="">
             Inscris toi !
           </Link>
