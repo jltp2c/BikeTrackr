f34e6a039b2b6fc37525cb56c2cfc891912c023d
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-09-22 17:12:39 +0200

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,29 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center h-screen">
-      <h1 className="h-24 font-bold text-6xl">BikeTrackr</h1>
-      <div className="flex gap-5 items-center ">
-        <Link href="/velos" className="border-2 border-red-700 ">
-          <Image src="/velo.png" alt="velo" height={100} width={100}></Image>
+      <h1 className="h-24 font-bold text-5xl sm:text-6xl">BikeTrackr</h1>
+      <div className="flex gap-5 items-center">
+        <Link
+          href="/velos"
+          className="border-8 border-grey-700 p-4 h-80 w-36 sm:w-56 md:h-96 relative">
+          <Image
+            src="/velo.png"
+            alt="velo"
+            fill
+            className="rotate-2 hover:rotate-0 transition-all"
+          />
         </Link>
-        <div className="flex flex-col border-2 border-red-700 gap-5 h-24 p-10">
-          <Link href="/signup" className="">
-            Inscris toi !
+        <div className="flex flex-col justify-center items-center border-8 border-grey-700 gap-5 h-80 w-36 p-4 sm:w-56 md:h-96 relative">
+          <Link
+            href="/signup"
+            className="border-2 p-2 rounded-md text-xs w-24 sm:w-32 text-center font-bold hover:text-green-600 hover:border-green-600 hover:scale-110 transition-all">
+            Inscription
           </Link>
-          <Link href="/login">Connecte-toi</Link>
+          <Link
+            href="/login"
+            className="border-2 p-2 rounded-md text-xs w-24 sm:w-32  text-center font-bold hover:text-green-600 hover:border-green-600 hover:scale-110 transition-all">
+            Connecte-toi
+          </Link>
         </div>
       </div>
     </main>
